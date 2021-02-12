88178620d3cfd1c05f960c4ad7f56f10712e2caa
iokoye <31423721+iokoye@users.noreply.github.com>
2021-02-11 22:42:06 -0500

    Add files via upload

---
+++ Lab11/Lab11/Movie.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab11
+{
+    public class Movie
+    {
+		public Movie(string title, string category)
+		{
+			_title = title;
+			_category = category;
+		}
+		public Movie()
+		{
+
+		}
+
+		private string _category;
+		private string _title;
+
+		public void GetAnimated()
+		{
+			var movielist = InitializeList();
+
+			foreach (var movie in movielist)
+			{
+				if (movie._category == "Animated")
+				{
+					Console.WriteLine(movie._title);
+				}
+			}
+		}
+
+		private List<Movie> InitializeList()
+		{
+			var movielist = new List<Movie>();
+			movielist.Add(new Movie("Spirited Away", "Animated"));
+			movielist.Add(new Movie("Frozen", "Animated"));
+			movielist.Add(new Movie("Rambo", "Action"));
+			movielist.Add(new Movie("Django", "Action"));
+
+			return movielist;
+		}
+
+	}
+}
+
---
+++ Lab11/Lab11/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var movie = new Movie();
+
+            movie.GetAnimated();            
+        }
+    }
+}
