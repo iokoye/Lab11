65aced60962f7b519f5ac2f776cae6bf0c84e79b
iokoye <31423721+iokoye@users.noreply.github.com>
2021-02-15 18:24:49 -0500

    Add files via upload

---
+++ Movie.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab11
+{
+    public class Movie
+    {
+		public Movie(string title, string category)
+		{
+			_title = title;
+			_category = category;
+		}
+		public Movie()
+		{
+
+		}
+
+		private string _category;
+		private string _title;
+
+		public void GetAnimated()
+		{				
+			var movielist = InitializeList();
+
+			foreach (var movie in movielist)
+			{			
+				if (movie._category == "Animated")
+				{
+					Console.WriteLine(movie._title);
+				}
+                
+				Console.WriteLine("Would you like to continue? (y/n)");
+				var response = Console.ReadLine();
+
+                if (response == "n")
+                {
+					break;
+                }
+
+			}
+		}
+
+		public void GetHorror()
+		{
+			var movielist = InitializeList();
+
+			foreach (var movie in movielist)
+			{
+				if (movie._category == "Horror")
+				{
+					Console.WriteLine(movie._title);
+				}
+
+				Console.WriteLine("Would you like to continue? (y/n)");
+				var response = Console.ReadLine();
+
+				if (response == "n")
+				{
+					break;
+				}
+
+			}
+		}
+
+		public void GetSciFi()
+		{
+			var movielist = InitializeList();
+
+			foreach (var movie in movielist)
+			{
+				if (movie._category.Equals("Sci Fi"));
+				{
+					Console.WriteLine(movie._title);
+				}
+
+				Console.WriteLine("Would you like to continue? (y/n)");
+				var response = Console.ReadLine();
+
+				if (response == "n")
+				{
+					break;
+				}
+
+			}
+		}
+
+		public void GetDrama()
+		{
+			var movielist = InitializeList();
+
+			foreach (var movie in movielist)
+			{
+				if (movie._category == "Drama")
+				{
+					Console.WriteLine(movie._title);
+				}
+
+				Console.WriteLine("Would you like to continue? (y/n)");
+				var response = Console.ReadLine();
+
+				if (response == "n")
+				{
+					break;
+				}
+
+			}
+		}
+
+
+
+		private List<Movie> InitializeList()
+		{
+			var movielist = new List<Movie>();
+			movielist.Add(new Movie("Spirited Away", "Animated"));
+			movielist.Add(new Movie("Frozen", "Animated"));
+			movielist.Add(new Movie("Star Trek", "Sci Fi"));
+			movielist.Add(new Movie("I, Robot", "Sci Fi"));
+			movielist.Add(new Movie("Lost in Translation", "Drama"));
+			movielist.Add(new Movie("Little Women", "Drama"));
+			movielist.Add(new Movie("Annabelle", "Horror"));
+			movielist.Add(new Movie("Saw", "Horror"));
+
+
+			return movielist;
+		}
+
+	}
+}
+
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movie = new Movie();
+
+
+
+            Console.WriteLine(GetUserInput());
+
+        }
+        public static string GetUserInput()
+        {
+            string response;
+            do
+            {
+                Console.WriteLine("Welcome to the Movie List Application!" + "\r\n" + "There are 100 movies in this list." + "\r\n" +
+              "What category are you interested in?");
+                response = Console.ReadLine();
+
+            } while (IsUserInputValid(response));
+
+            return response;
+        }
+        public static bool IsUserInputValid(string response)
+        {
+            if (response == "Sci Fi")
+            {
+                var scifi = new Movie();
+                scifi.GetSciFi();
+            }           
+            return true;
+        }
+    }
+}
